4c6a1f39f81f5da611e56e1ef1228b339339c758
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2021-11-06 23:11:18 -0400

    Add `export as namespace` to declarations
    Adds a Node script to add `export as namespace GreaseTools` to the
    generated type declaration files. This allows GreaseTools to be added as
    a dev dependency to a UserScript that includes the library with
    `@require`.

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,2 +1,4 @@
 export * from './banner';
 export * from './values';
+
+export as namespace GreaseTools;
---
+++ updatedts.js
@@ -0,0 +1,16 @@
+const { readFile, appendFile } = require('fs')
+const root = require('path').dirname(
+  require.main.filename || process.mainModule.filename
+)
+
+const exportAsNamespace = '\nexport as namespace GreaseTools;\n'
+
+console.log('Project Root:', root)
+
+readFile(`${root}/lib/index.d.ts`, (err, data) => {
+  if (err) throw err
+  if (!data.includes(exportAsNamespace))
+    appendFile(`${root}/lib/index.d.ts`, exportAsNamespace, 'utf-8', err => {
+      if (err) throw err
+    })
+})
