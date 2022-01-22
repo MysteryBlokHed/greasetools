a0a307f3bb083b45e65b622be2de7d75172f0aea
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2022-01-22 13:11:40 -0500

    Update banner.ts function & types
    Adds the option to provide arbitrary keys to the banner. Also adds a
    paremeter to change from the default UserScript header (eg. for
    userscript library headers).

--- src/banner.ts
+++ src/banner.ts
@@ -1,3 +1,5 @@
+type MetadataValue = string | readonly (string | undefined)[] | undefined
+
 export type MetadataObject = Partial<
   Record<
     | 'description'
@@ -13,9 +15,10 @@ export type MetadataObject = Partial<
     | 'resource'
     | 'run-at'
     | 'version',
-    string | readonly (string | undefined)[] | undefined
+    MetadataValue
   >
->
+> &
+  Record<string, MetadataValue>
 
 /**
  * Generate a UserScript metadata comment from an object.
@@ -25,11 +28,18 @@ export type MetadataObject = Partial<
  * @param metaValues Properties to add to metadata
  * @param spacing The amount of spaces between the `@` and the value, including the prop name.
  * Should be at least 1 greater than the longest prop name
+ * @param start What to put at the start of the banner. Defaults to `'// ==UserScript=='`
+ * @param end What to put at the end of the banner. Defaults to `'// ==/UserScript=='`
  * @returns A block of comments to be put at the top of a UserScript
  * including all of the properties passed
  */
-export function genBanner(metaValues: MetadataObject, spacing = 12): string {
-  let final = '// ==UserScript==\n'
+export function genBanner(
+  metaValues: MetadataObject,
+  spacing = 12,
+  start = '// ==UserScript==',
+  end = '// ==/UserScript==',
+): string {
+  let final = `${start}\n`
 
   const format = (prop: string, value: string) =>
     `// @${prop}${' '.repeat(spacing - prop.length)}${value}\n`
@@ -47,6 +57,6 @@ export function genBanner(metaValues: MetadataObject, spacing = 12): string {
     }
   }
 
-  final += '// ==/UserScript==\n'
+  final += `${end}\n`
   return final
 }
